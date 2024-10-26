ecf02dce427082a8931045f0cf9d67af1502546c
Jakub Wierzchowiec <wierzchowieckuba@gmail.com>
2024-10-26 22:38:52 +0200

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/913aIS7nnu

---
+++ apps/dentist-appointment-booking-v2-server/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'dentist-appointment-booking-v2-server',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/dentist-appointment-booking-v2-server'
+};
---
+++ apps/dentist-appointment-booking-v2-server/webpack.config.js
@@ -0,0 +1,22 @@
+
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const { join } = require('path');
+
+module.exports = {
+  output: {
+    path: join(__dirname, '../../dist/apps/dentist-appointment-booking-v2-server'),
+  },
+  plugins: [
+    new NxAppWebpackPlugin({
+      target: 'node',
+      compiler: 'tsc',
+      main: './src/main.ts',
+      tsConfig: './tsconfig.app.json',
+      assets: ["./src/assets"],
+      optimization: false,
+      outputHashing: 'none',
+      generatePackageJson: true,
+    })
+  ],
+};
+
